3ef44fb23687b406294b8185b54ec5af678a0240
DavidJi80 <jz_1106@163.com>
2018-11-26 14:21:49 +0800

    Android：Activity的生命周期

--- app/src/main/java/com/github/davidji80/helloworld/ClockActivity.java
+++ app/src/main/java/com/github/davidji80/helloworld/ClockActivity.java
@@ -1,9 +1,12 @@
 package com.github.davidji80.helloworld;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -13,12 +16,14 @@ import java.util.TimerTask;
 
 public class ClockActivity extends AppCompatActivity {
 
+    private static String ClASS_NAME="ClockActivity";
+
     protected TextView counter;
     protected Button start;
     protected Button stop;
 
     //两个按钮互斥变量
-    protected boolean timerRunning;
+    protected boolean timerRunning=false;
     //时间变量
     protected long startedAt;
     protected long lastStopped;
@@ -54,9 +59,11 @@ public class ClockActivity extends AppCompatActivity {
     private UpdateTimer updateTimer;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(ClASS_NAME,"----------------onCreate----------------");
         setContentView(R.layout.activity_clock);
         counter = (TextView) findViewById(R.id.timer);
         start = (Button) findViewById(R.id.start_button);
@@ -66,19 +73,24 @@ public class ClockActivity extends AppCompatActivity {
     public void clickedStart(View view) {
         timerRunning = true;
         enableButtons();
-        startedAt = System.currentTimeMillis();
-        lastStopped = System.currentTimeMillis();
         updateTimer=new UpdateTimer();
         timer.schedule(updateTimer, 0, 200);
+        startedAt = System.currentTimeMillis();
+        lastStopped = System.currentTimeMillis();
+        saveState();
+    }
+
+    public void saveState(){
+        SharedPreferences sharedPref=this.getSharedPreferences("SAVEDSTART",Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor=sharedPref.edit();
+        editor.putBoolean("timerRunning",timerRunning);
+        editor.commit();
     }
 
     public void clickedStop(View view) {
         timerRunning = false;
         enableButtons();
-        startedAt = System.currentTimeMillis();
-        lastStopped = System.currentTimeMillis();
         handler.removeCallbacks(updateTimer);
-        //handler = null;
     }
 
     protected void enableButtons() {
@@ -109,5 +121,66 @@ public class ClockActivity extends AppCompatActivity {
         minutes = minutes % 60;
         display = String.format("%d", hours) + ":" + String.format("%02d", minutes) + ":" + String.format("%02d", seconds);
         counter.setText(display);
+        Log.d("startedAt",Long.toString(startedAt));
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(ClASS_NAME,"----------------onStart----------------");
+        SharedPreferences sp=this.getSharedPreferences("SAVEDSTART",Context.MODE_PRIVATE);
+        timerRunning=sp.getBoolean("timerRunning",false);
+        if (timerRunning){
+            updateTimer=new UpdateTimer();
+            timer.schedule(updateTimer, 0, 200);
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(ClASS_NAME,"----------------onResume----------------");
+        enableButtons();
+        setTimeDisplay();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(ClASS_NAME,"----------------onPause----------------");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(ClASS_NAME,"----------------onStop----------------");
+        if (timerRunning){
+            handler.removeCallbacks(updateTimer);
+        }
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(ClASS_NAME,"----------------onStart----------------");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        android.os.Debug.stopMethodTracing();
+        Log.d(ClASS_NAME,"----------------onDestroy----------------");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putLong("startedAt",startedAt);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        startedAt=savedInstanceState.getLong("startedAt");
     }
 }
